3a8526618404e6c6759512a0ed6779c3270950fa
Muhammad Talha <talhagp908@users.noreply.github.com>
2025-05-14 20:41:25 +0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header/Header";
-import Image from "next/image";
-import heroImage from "@/images/herobg.png";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
